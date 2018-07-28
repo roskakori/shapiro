061bb755e26da1eb8ba81a101ab3184885633eb6
alex <machado.alexander@gmail.com>
2018-07-28 17:48:46 +0100

    english sentiment static methods. Language sentiment static methods

--- src/shapiro/common.py
+++ src/shapiro/common.py
@@ -16,6 +16,20 @@ class Rating(Enum):
 _MIN_RATING_VALUE = Rating.VERY_BAD.value
 _MAX_RATING_VALUE = Rating.VERY_GOOD.value
 
+RATING_TO_NEGATED_RATING_MAP = {
+    Rating.VERY_BAD     : Rating.SOMEWHAT_GOOD,
+    Rating.BAD          : Rating.GOOD,
+    Rating.SOMEWHAT_BAD : Rating.GOOD,  # hypothetical?
+    Rating.SOMEWHAT_GOOD: Rating.BAD,  # hypothetical?
+    Rating.GOOD         : Rating.BAD,
+    Rating.VERY_GOOD    : Rating.SOMEWHAT_BAD,
+}
+
+
+def negated_rating(rating: Rating) -> Rating:
+    assert rating is not None
+    return RATING_TO_NEGATED_RATING_MAP[rating]
+
 
 def ranged_rating(rating_value: int) -> Rating:
     return Rating(min(_MAX_RATING_VALUE, max(_MIN_RATING_VALUE, rating_value)))
